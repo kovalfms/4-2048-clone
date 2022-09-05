409f5eec23eb69c91a857a1255984fad9748e2e3
kovalfms <famouscossacks@gmail.com>
2022-09-05 12:39:54 +0300

    add popup / fixed styles (DVP-01)

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import classes from './Popup.module.css'
 
 export const Popup = ({text, resetGame}) => {
-    console.log(text)
     return (
         <div className={classes.popup}>
             <div className={classes.popup_inner}>
